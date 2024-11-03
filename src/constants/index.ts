bc3463629cb21660eb5e94a6ac1b81113575aae7
minhcu <minhdong1831999@gmail.com>
2024-11-03 14:02:31 +0700

    feat: update breakline

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,56 +1,56 @@
 export const articles = [
   {
-    title: "Sinh nhật S-Group 2023",
+    title: "Sinh nhật\nS-Group 2023",
     image: "./images/image-1.jpg",
     url: "https://www.facebook.com/share/p/ZKMDbK34krNeM1FC/"
   },
   {
-    title: "Demo day - Dự án cuối khoá khuyên môn Lập Trình",
+    title: "Demo day\nDự án cuối khoá\nchuyên môn Lập Trình",
     image: "./images/image-2.jpg",
     url: "https://www.facebook.com/share/p/R5hEBvqqmqQcCgef/"
   },
   {
-    title: "Talkshow \"Nghe chuyện người trong ngành\" ",
+    title: "Talkshow\n\"Nghe chuyện\nngười trong ngành\" ",
     image: "./images/image-3.jpg",
     url: "https://www.facebook.com/share/p/Nn5iLgazTL8wcz7d/"
   },
   {
-    title: "Future Ctrl | S-group chính thức tuyển thành viên khoá mùa đông 2023",
+    title: "Future Ctrl\nS-group chính thức\ntuyển thành viên\nkhoá mùa đông 2023",
     image: "./images/image-4.jpg",
     url: "https://www.facebook.com/share/p/chPguL8gWveSPCMN/"
   },
   {
-    title: "S-Group tham gia company tour tại Axon Active",
+    title: "S-Group\ntham gia\ncompany tour\ntại Axon Active",
     image: "./images/image-5.jpg",
     url: "https://www.facebook.com/share/p/bqZsdBr7swUDYK1f/"
   },
   {
-    title: "S-Group Shuttle Up 2024",
+    title: "S-Group\nShuttle Up 2024",
     image: "./images/image-6.jpg",
     url: "https://www.facebook.com/share/p/sbdXV3FCzYTfWFsp/",
   },
   {
-    title: "Chuyên môn Marketing Online tốt nghiệp khoá học content",
+    title: "Chuyên môn\nMarketing Online\ntốt nghiệp\nkhoá học content",
     image: "./images/image-7.jpg",
     url: "https://www.facebook.com/share/p/JDVucN3miVwRs9Fd/"
   },
   {
-    title: "Chuyên môn Thiết Kế trình bày đồ án",
+    title: "Chuyên môn\nThiết Kế\ntrình bày đồ án",
     image: "./images/image-8.jpg",
     url: "https://www.facebook.com/share/p/WVB3a6nzHXDEKGaE/"
   },
   {
-    title: "Thành viên S-Group tham gia thi tại Unihack 2024",
+    title: "Thành viên\nS-Group\ntham gia thi\ntại Unihack 2024",
     image: "./images/image-9.jpg",
     url: "https://www.facebook.com/share/p/QnZgEurUQfFHrXMN/"
   },
   {
-    title: "Workshop \"Khám phá tiềm năng và cơ hội việc làm ngành Salesforce\"",
+    title: "Workshop\n\"Khám phá tiềm năng\nvà cơ hội việc làm\nngành Salesforce\"",
     image: "./images/image-10.jpg",
     url: "https://www.facebook.com/share/p/rTu33HVarRmbhvAH/"
   },
   {
-    title: "Trung thu \"Vẽ màu trăng mới\" cho các em nhỏ tại Hoà Bắc",
+    title: "Trung thu\n\"Vẽ màu trăng mới\"\ncho các em nhỏ\ntại Hoà Bắc",
     image: "./images/image-11.jpg",
     url: "https://www.facebook.com/share/p/UAWD6bhjRggUy4bf/"
   },

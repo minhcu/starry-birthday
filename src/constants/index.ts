886c5c041401049f5a2c5b1affdc63b4d13cfd6e
minhcu <minhdong1831999@gmail.com>
2024-11-03 13:05:33 +0700

    feat: add images and url

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,47 +1,58 @@
 export const articles = [
   {
-    title: "1. This is 5 word title",
-    image: "./images/test-image.jpg",
+    title: "Sinh nhật S-Group 2023",
+    image: "./images/image-1.jpg",
+    url: "https://www.facebook.com/share/p/ZKMDbK34krNeM1FC/"
   },
   {
-    title: "2. This is 5 word title",
-    image: "./images/test-image.jpg",
+    title: "Demo day - Dự án cuối khoá khuyên môn Lập Trình",
+    image: "./images/image-2.jpg",
+    url: "https://www.facebook.com/share/p/R5hEBvqqmqQcCgef/"
   },
   {
-    title: "3. This is 5 word title",
-    image: "./images/test-image.jpg",
+    title: "Talkshow \"Nghe chuyện người trong ngành\" ",
+    image: "./images/image-3.jpg",
+    url: "https://www.facebook.com/share/p/Nn5iLgazTL8wcz7d/"
   },
   {
-    title: "4. This is 5 word title",
-    image: "./images/test-image.jpg",
+    title: "Future Ctrl | S-group chính thức tuyển thành viên khoá mùa đông 2023",
+    image: "./images/image-4.jpg",
+    url: "https://www.facebook.com/share/p/chPguL8gWveSPCMN/"
   },
   {
-    title: "5. This is 5 word title",
-    image: "./images/test-image.jpg",
+    title: "S-Group tham gia company tour tại Axon Active",
+    image: "./images/image-5.jpg",
+    url: "https://www.facebook.com/share/p/bqZsdBr7swUDYK1f/"
   },
   {
-    title: "6. This is 5 word title",
-    image: "./images/test-image.jpg",
+    title: "S-Group Shuttle Up 2024",
+    image: "./images/image-6.jpg",
+    url: "https://www.facebook.com/share/p/sbdXV3FCzYTfWFsp/",
   },
   {
-    title: "7. This is 5 word title",
-    image: "./images/test-image.jpg",
+    title: "Chuyên môn Marketing Online tốt nghiệp khoá học content",
+    image: "./images/image-7.jpg",
+    url: "https://www.facebook.com/share/p/JDVucN3miVwRs9Fd/"
   },
   {
-    title: "8. This is 5 word title",
-    image: "./images/test-image.jpg",
+    title: "Chuyên môn Thiết Kế trình bày đồ án",
+    image: "./images/image-8.jpg",
+    url: "https://www.facebook.com/share/p/WVB3a6nzHXDEKGaE/"
   },
   {
-    title: "9. This is 5 word title",
-    image: "./images/test-image.jpg",
+    title: "Thành viên S-Group tham gia thi tại Unihack 2024",
+    image: "./images/image-9.jpg",
+    url: "https://www.facebook.com/share/p/QnZgEurUQfFHrXMN/"
   },
   {
-    title: "10. This is 5 word title",
-    image: "./images/test-image.jpg",
+    title: "Workshop \"Khám phá tiềm năng và cơ hội việc làm ngành Salesforce\"",
+    image: "./images/image-10.jpg",
+    url: "https://www.facebook.com/share/p/rTu33HVarRmbhvAH/"
   },
   {
-    title: "11. This is 5 word title",
-    image: "./images/test-image.jpg",
+    title: "Trung thu \"Vẽ màu trăng mới\" cho các em nhỏ tại Hoà Bắc",
+    image: "./images/image-11.jpg",
+    url: "https://www.facebook.com/share/p/UAWD6bhjRggUy4bf/"
   },
 ];
 
